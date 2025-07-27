bdbc9d92a4ee061c620c90aae9738cbc535f3c13
Matt <steffen.matthewj@gmail.com>
2025-07-27 15:46:28 -0700

    finishing up tool servers, need to test

--- backend/setup.md
+++
@@ -1,6 +0,0 @@
-# TODO
-
-## OI
-
-- Enable tool calling again, treat the search and query as tools from OI, I think there is a __tools__ parameter
-- In helm charts, you can set the database to be milvus with own URI.
---
+++ backend/tools_server/tools/comb/tools.py
@@ -0,0 +1,36 @@
+from .schemas import CombInput
+from ..milvus_search.utils import connect_milvus
+import ollama
+
+
+def comb(input_data: CombInput):
+    milvus_client = connect_milvus()
+    if not milvus_client:
+        raise ValueError("cannot connect to milvus")
+
+    # 1. Query for all documents with a given filter
+    query_results = milvus_client.query(
+        collection_name="test_collection",
+        filter=" and ".join(input_data.filters) if input_data.filters else "",
+        output_fields=["text"],  # We only need the text for the LLM
+        limit=1000,  # Get a large number of documents to comb through
+    )
+
+    # 2. Ask LLM to read and give its thoughts
+    combined_text = "\n".join([res["text"]] for res in query_results["results"])
+    prompt = f"From the perspective of {input_data.perspective}, read the following documents and provide your thoughts and insights:\n\n{combined_text}"
+
+    try:
+        client = ollama.Client(host="http://localhost:11434")
+        response = client.chat(
+            model="o1-preview",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+        )
+        return {"thoughts": response["message"]["content"]}
+    except Exception as e:
+        raise ValueError("error communicating with ollama")
